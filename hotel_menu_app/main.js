001c46ed0e032df3e0cd83b6b2c154e90ac049de
bharathitanniru <bharathi.bhargav@gmail.com>
2023-06-19 13:21:05 +0530

    updated

--- hotel_menu_app/main.js
+++ hotel_menu_app/main.js
@@ -1,36 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Project - 79</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="main.js"></script>
-</head>
-<body>
-    <img src="https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-</body>
-</html>
-<center>
-<h3 style="margin-top: 20px; margin-bottom: 20px; color: green;">
-Menu List Of the cafe
-</h3>
+menu_list_items = ["Chicken Tandoori", "Veg Supreme Burger", "Paneer Tikka", "Chicken Chili", "Cheese Burst Pizza, sushi, dimsums"];
 
-<div id="menu_list_items">
-	
-</div>
+function run()
+{
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
 
-<button onclick="run();">Menu List</button>
+function add_new_menu_item()
+{
+    new_menu_item = document.getElementById("new_menu_item").value;
+    menu_list_items.push(new_menu_item);
+    menu_list_items.sort();
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
 
-<div>
-	<span>Add New Menu Item - </span>
-	<input type="text" placeholder="New Menu Item" id="new_menu_item">
-</div>
-
-<button onclick="add_new_menu_item();">Add New Menu Item</button>
-
-</center>
-
-<script>
-
-</script>
-</body>
-</html>
